7ff51c668b44fd5d27ed25bf475c2274d6920f87
ItchelZuniga <itchelzng@gmail.com>
2023-01-19 14:39:37 -0600

    login-commit

--- fan_page/src/index.js
+++ fan_page/src/index.js
@@ -6,10 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><Auth0Provider 
+  domain="dev-qbm887swakctegsl.us.auth0.com" 
+  clientId='oFTPPBwzGyRgDfuFFx1SoV2tCIKsjb5L' 
+  redirectUri={window.location.origin}> 
+  <App />
+  </Auth0Provider>
+</React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
